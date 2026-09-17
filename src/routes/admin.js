80f0d0e6a24aaa33d85b777eab2c122e987d2ea0
LilTeam Shop <slamdone5@gmail.com>
2026-09-18 04:46:19 +0700

    Use generic storefront model title

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -917,7 +917,7 @@ router.post('/home-sections/:id/toggle', async (req, res) => {
 // ---------- Storefront models ----------
 router.get('/storefront-models', (req, res) => {
   res.render('admin/storefront-models', {
-    title: 'โมเดลหน้าร้าน LINE Rangers',
+    title: 'โมเดลหน้าร้าน',
     active: 'storefront-models',
     currentModel: store.data.settings.storefrontModel || 'classic',
     allowRangersMarket: !!req.tenantShop?.isSystemLab,
